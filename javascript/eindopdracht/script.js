3b5b03f3f1a9643da8e1ceefed90caddf33ec0c7
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-03-17 11:18:02 +0100

    working open popup and close popup, and made an array for games that are added after fetch call is completed

--- javascript/eindopdracht/script.js
+++ javascript/eindopdracht/script.js
@@ -1,4 +1,36 @@
+// variables //
+const arrGames = [];
+const overlay = document.querySelector(".overlay");
+const hidden = document.querySelector(".hidden");
+const popup = document.querySelector(".popup");
+const gamePicker = document.getElementById("game-picker");
+///////
+
+// fetch call to the json file //
 fetch("./assets/games.json")
   .then((response) => response.json())
-  .then((games) => console.log(games))
+  .then((games) => arrGames.push(games))
   .catch((error) => console.error("Fout bij laden JSON:", error));
+//
+
+// Popup functions //
+const openPopup = function () {
+  popup.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closePopup = function () {
+  popup.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+gamePicker.addEventListener("click", () => openPopup());
+overlay.addEventListener("click", () => closePopup());
+
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+  if (event.key === "Escape" && !popup.classList.contains("hidden")) {
+    closePopup();
+  }
+});
+////////
